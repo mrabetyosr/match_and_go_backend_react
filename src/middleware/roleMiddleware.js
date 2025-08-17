160cee40169653368937fb28d30dd9da7b51a77f
Yosr Mrabet <yosr.mrabet@esprit.tn>
2025-08-17 04:21:36 +0100

    Implement Role-Based Authorization

---
+++ src/middleware/roleMiddleware.js
@@ -0,0 +1,14 @@
+const authorizeRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!allowedRoles.includes(req.user.role)) {
+            return res
+            .status(403)
+            .json({ message: "Acess denied" });
+        }
+
+       next();
+    };
+
+}
+
+module.exports = authorizeRoles;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const verifyToken = require("../middleware/authMiddleware");
-
+const authorizeRoles = require("../middleware/roleMiddleware");
 const router = express.Router();
 
 //only admin can access this route
-router.get("/admin", verifyToken, (req, res) => {
-    res.json({ message: "welcom admin" });
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
+    res.json({ message: "welcome admin" });
 
 });
 //only admin and recruter  can access this route
-router.get("/company", verifyToken, (req, res) => {
-    res.json({ message: "welcom company" });
+router.get("/company", verifyToken, authorizeRoles("company", "admin"), (req, res) => {
+    res.json({ message: "welcome company" });
 });
 
 //all can access this route
-router.get("/candidate", verifyToken, (req, res) => {
-    res.json({ message: "welcom candidate" });
+router.get("/candidate", verifyToken, authorizeRoles("candidate", "admin", "company"), (req, res) => {
+    res.json({ message: "welcome candidate" });
 });
 
 
