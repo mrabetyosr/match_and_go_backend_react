142d5e72984d9f9ae0714763e247d8b314ec7523
Yosr Mrabet <yosr.mrabet@esprit.tn>
2025-08-26 01:48:42 +0100

    create interview model

---
+++ src/models/interviewModel.js
@@ -0,0 +1,25 @@
+// models/interviewModel.js
+const mongoose = require("mongoose");
+
+const interviewSchema = new mongoose.Schema(
+  {
+    applicationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Application",
+      required: true,
+    },
+    scheduledBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: { type: Date, required: true },
+    meetLink: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports =
+  mongoose.models.Interview ||
+  mongoose.model("interviewModel", interviewSchema);
