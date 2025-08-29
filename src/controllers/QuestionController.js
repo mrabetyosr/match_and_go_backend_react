f12472b054f3b0a973f0eab2c4f6a88357a66452
Yosr Mrabet <yosr.mrabet@esprit.tn>
2025-08-29 21:05:49 +0100

    update question score and recalc totalScore of quiz

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -86,6 +86,7 @@ exports.updateQuestion = async (req, res) => {
     if (questionType) question.questionType = questionType;
 
     await question.save();
+    await recalcQuizStats(question.quiz); 
     res.status(200).json(question);
   } catch (err) {
     res.status(500).json({ message: err.message });
